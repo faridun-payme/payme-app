34d04691f7da0f821888195572050c08260c322a
boronov <faridun.b@payme.uz>
2025-01-30 16:11:22 +0500

    create pull-build-logic

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,8 +17,33 @@ dependencyResolutionManagement {
         google()
         mavenCentral()
     }
+
+    versionCatalogs {
+        create("libs") {
+            from(files("build-logic/gradle/libs.versions.toml"))
+        }
+    }
 }
+
+// Path to the script
+val script = File(rootDir, "pull-build-logic.sh")
+
+// Run script before including build-logic
+if (script.exists() && script.canExecute()) {
+    println("Running pull-build-logic.sh before project sync...")
+    val process = ProcessBuilder(script.absolutePath)
+        .inheritIO()
+        .start()
+    val exitCode = process.waitFor()
+
+    if (exitCode != 0) {
+        error("pull-build-logic.sh failed with exit code: $exitCode")
+    }
+} else {
+    println("Script not found or not executable: $script")
+}
+
+includeBuild("build-logic")
 
 rootProject.name = "PaymeSuperApp"
 include(":app")
- 
